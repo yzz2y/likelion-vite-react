ffc5fcfb443a4e69d670deefc7deb3d047643767
nurungzzy <duswl0801@khu.ac.kr>
2025-01-17 15:07:27 +0900

    [구성] 관심사의 분리 (컴포넌트 중심 설계)
    - app 디렉토리 추가
    - app.jsx 컴포넌트
    - app.css 스타일

---
+++ src/app/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="App">{props.children}</div>
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/main.css';
+import App from './app/app';
 
 const root = document.getElementById('root');
 
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <h1 className='hello'>안녕!</h1>
+      <App>
+        <h1>Hello React</h1>
+      </App>
     </StrictMode>
   )
 }
